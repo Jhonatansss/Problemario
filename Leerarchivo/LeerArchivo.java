370ccd48c3c8ad49e229f9235a9f3b13f3a4b813
desarrollador3he <desarrollador3he>
2023-11-10 16:06:50 -0600

    Actua

--- Leerarchivo/LeerArchivo.java
+++ Leerarchivo/LeerArchivo.java
@@ -1,2 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+
 public class LeerArchivo {
+    public static void main(String[] args) {
+        try {
+            String rutaArchivo = "datos.txt";
+
+            FileReader archivo = new FileReader(rutaArchivo);
+
+            BufferedReader lector = new BufferedReader(archivo);
+
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+            lector.close();
+        } catch (FileNotFoundException e) {
+
+            System.err.println("Error: Archivo no encontrado. Verifica la ruta y el nombre del archivo.");
+        } catch (IOException e) {
+
+            System.err.println("Error de entrada/salida al leer el archivo.");
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
